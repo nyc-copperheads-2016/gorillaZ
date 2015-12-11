da9305dfe3f27f33935fb086fdb459fbd525b7a2
Courtney & Chris Savage <pair+aquarian5.chrislsavage@devbootcamp.com>
2015-12-11 18:45:27 -0500

    user controller routes are added; User Authentication implemented

--- db/migrate/20151211144921_create_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateUser < ActiveRecord::Migration
-  def change
-    create_table  do |t|
-      t.string :username, null: false
-      t.string :password_hash, null: false
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20151211144946_create_questions.rb
+++ db/migrate/20151211144946_create_questions.rb
@@ -2,8 +2,9 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table  do |t|
       t.string :description
-      t.timestamps null: false
       t.belongs_to :survey, null: false
+
+      t.timestamps null: false
     end
   end
 end
