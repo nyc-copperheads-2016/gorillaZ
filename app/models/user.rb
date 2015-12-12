75f066e8df181b56a5558686fb76e2cf8a65d60a
Bruno Zatta <bzatta@gmail.com>
2015-12-12 14:22:38 -0500

    Fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ validates :username, presence:true, uniqueness: true
 validates :password_hash, presence: true, length: {minimum: 6}
 
     def password
-    @password || Password.new(password_hash)
+    @password ||= Password.new(password_hash)
   end
 
   def password=(new_password)
