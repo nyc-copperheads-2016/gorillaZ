0340b6026b0aa2ee175b0270538f5d636b7ded08
Bruno Zatta <bzatta@gmail.com>
2015-12-11 20:00:37 -0500

    Updated nameing of routes and variables

--- app/controllers/taken_surveys_controller.rb
+++ app/controllers/taken_surveys_controller.rb
@@ -1,11 +1,11 @@
-get '/survey/:id' do
+get '/taken_survey/:id' do
   survey = TakenSurvey.new(survey_id: params[:id], user_id: current_user.id)
   initial_question = Survey.find(params[:id]).first
   if survey.save
     erb :'/question/show', locals: {survey: survey, initial_question: initial_question}
   else
     errors = survey.errors.full_messages
-    erb :'/'
+    erb :'/', locals: {errors: errors}
   end
 end
 
