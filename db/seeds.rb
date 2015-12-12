b033f299adcc60db66e6d1b0cfa8eec79eeb6bfb
Courtney <email": null>
2015-12-12 17:04:28 -0500

    Add an additional seed file to surveys; stands as our test survey

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,17 +26,31 @@ taken_survey1.answers.create( question_id:1 , choice_id:1 )
 taken_survey1.answers.create( question_id:2 , choice_id:1 )
 taken_survey1.answers.create( question_id:3 , choice_id:1 )
 
+user2 = User.create(username: "Courtney", password: "nation")
 
-# courtney = User.create([{username: "Courtney", password: "123456789"}])
+survey2 = user2.surveys.create(name: "What Do You Like To Do In Your Free Time?")
 
-# freetime_survey = courtney << Survey.new({name: "What Do You Like To Do In Your Free Time?"})
 
-# freetime_survey << Question.new({description: "Do You Like To Travel?", answer: "Yes", answer: "No", answer: "Unsure"})
+question1 = survey2.questions.create(description: "Do You Like To Socialize?")
+question2 = survey2.questions.create(description: "Do You Like To Relax And Rest?")
+question3 = survey2.questions.create(description: "Do You Like To Eat?")
 
-# freetime_survey << Question.new({description: "Do You Like To Socialize?"})answer: "Yes", answer: "No", answer: "Unsure"})
-# freetime_survey << Question.new({description: "Do You Like To Relax And Rest?", answer: "Yes", answer: "No", answer: "Unsure"})
-# freetime_survey << Question.new({description: "Do You Like To Eat?", answer: "Yes", answer: "No", answer: "Unsure"})
-# freetime_survey << Question.new({description: "Do You Have Many Friends?", answer: "Yes", answer: "No", answer: "Unsure"})
-# freetime_survey.save
+question1.choices.create(answer: "Yes")
+question1.choices.create(answer: "No")
+question1.choices.create(answer: "Unsure")
+
+
+question2.choices.create(answer: "Yes")
+question2.choices.create(answer: "No")
+question2.choices.create(answer: "Unsure")
+
+
+question3.choices.create(answer: "Yes")
+question3.choices.create(answer: "No")
+question3.choices.create(answer: "Unsure")
+
+taken_survey2.answers.create( question_id:1 , choice_id:1 )
+taken_survey2.answers.create( question_id:2 , choice_id:1 )
+taken_survey2.answers.create( question_id:3 , choice_id:1 )
 
 
