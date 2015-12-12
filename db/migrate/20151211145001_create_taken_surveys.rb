7c0a10bcc2d8238605befb31baed1ec0b094e96d
will van wart <william@wvwproductions.com>
2015-12-11 20:30:49 -0500

    add table names : )

--- db/migrate/20151211145001_create_taken_surveys.rb
+++ db/migrate/20151211145001_create_taken_surveys.rb
@@ -1,6 +1,7 @@
 class CreateTakenSurveys < ActiveRecord::Migration
   def change
-    create_table  do |t|
+    create_table :taken_surveys do |t|
+      t.timestamps null: false
       t.belongs_to :user
       t.belongs_to :surveys
 
