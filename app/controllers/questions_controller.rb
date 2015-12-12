6e01a469d6cd26021b5dedde234bc1f57a326f49
Bruno Zatta <bzatta@gmail.com>
2015-12-11 19:30:50 -0500

    Update controllers fur survey and question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,10 +14,10 @@ post '/questions' do
   answer = Answer.new(params)
   if answer.save
     # question id is static here so no way of adding a method to the model. It will need to be manually incremented (+1) here or in the view
-    redirect '/questions/<%= params[:question.id] %>'
-  else
-    error = answer.errors.full_messages
-    erb :'/questions/show', locals: {answer: answer}
+    redirect '/questions/<%= params[:question.id] %> + 1'
+  # else
+    # error = answer.errors.full_messages
+    # erb :'/questions/show', locals: {answer: answer}
   end
 end
 
