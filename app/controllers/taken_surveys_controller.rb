4781db28aa1ca2e864a6f76eee9871d2641b8d00
Chris Savage <email": null>
2015-12-12 14:16:39 -0500

    new_user view

--- app/controllers/taken_surveys_controller.rb
+++ app/controllers/taken_surveys_controller.rb
@@ -1,6 +1,6 @@
 get '/taken_survey/:id' do
   taken_survey = TakenSurvey.new(survey_id: params[:id], user_id: current_user.id)
-  initial_question = Survey.find(params[:id])questions.first
+  initial_question = Survey.find(params[:id]).questions.first
   if taken_survey.save
     redirect 'taken_survey/:{#taken_survey.id}/questions/<%= initial_question.id %>'
   else
