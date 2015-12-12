1ea4388874b2f2af63be3f948cb5164ceaf91b0f
Chris Savage <email": null>
2015-12-12 13:45:20 -0500

    add survey/new and controller files

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,11 @@
 class Choice < ActiveRecord::Base
   belongs_to :question
   has_many :answers
+
+  def self.create_choices(question, choice_answers)
+    choice_answers = choice_answers.split(",")
+    choice_answers.each do |choice_answer|
+      question.choices.new(answer: choice_answer)
+    end
+  end
 end
